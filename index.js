74121f26c8290eb270774a5ef11a02d46921359b
Miguel Sena <miguelaugustosena@gmail.com>
2021-09-08 20:31:18 -0300

    Htmls agora informando dados da api

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
-const express = require("express");
+//Requisitando todas as bibliotecas 
+import express, { response } from "express";
 const app = express();
-const handlebars = require("express-handlebars");
+import handlebars from "express-handlebars";
+import fetch from "node-fetch";
 
 //config
     //template engine
     app.engine('handlebars', handlebars({defaultLayout:"main"}));
     app.set("view engine","handlebars");
 
+//abrindo página inicial
 app.get("/", (req,res)=>{
-    res.render("pagina_inicial")
+    res.render("pagina_inicial");
 });
 
+//abrindo albums
+app.get("/albums", (req,res)=>{
+    fetch("https://jsonplaceholder.typicode.com/albums")
+    .then(response=> response.json())
+    .then(data=>{
+        res.render("albums", {albums:data})
+    })
+});
+
+//abrindo posts
+app.get("/posts", (req,res)=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response=> response.json())
+    .then(data=>{
+        res.render("posts", {posts:data})
+    })
+});
+
+//abrindo to-dos
+app.get("/to-dos", (req,res)=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(response=> response.json())
+    .then(data=>{
+        res.render("todos", {todos:data})
+    })
+});
 
 //Inicializando aplicação
 app.listen(8080, ()=>{
-    console.log("Rodando na porta 8080")
+    console.log("Rodando na porta 8080");
 });
-
