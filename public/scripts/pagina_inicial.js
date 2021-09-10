cc80237c188603c215d7a1478c26ed0c6d804188
Miguel Sena <miguelaugustosena@gmail.com>
2021-09-10 17:07:59 -0300

    Página inicial passando por modificações.

--- public/scripts/pagina_inicial.js
+++ public/scripts/pagina_inicial.js
@@ -3,3 +3,11 @@ $(document).ready(function() {
 	$(".navbar").hide()
 	$(".footer").hide()
 });
+
+$(".hpb").hover(function () {
+		// over
+	$(".section2").css("background-color" , 'rgba(0,0,0,0.5)');
+	}, function () {
+		$(".section1").css("transition(0.4)")
+	}
+);
